91db7e092a6958b1b2de7400540188fec524b871
luiza <luizamfelix@gmail.com>
2023-03-14 17:52:54 -0300

    Atualização 14/03/2023

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,35 @@
+/*
+RESUMO      : Exercício 05, Execução das Ordenações
+PROGRAMADORA: Luiza Felix
+DATA        : 14/03/2023
+ */
+
 package controller;
 
+import br.edu.fateczl.ordenacao.BubbleSort;
+import br.edu.fateczl.ordenacao.MergeSort;
+
 public class Controller {
+	public Controller() {
+		super();
+	}
+
+	public void ordem(int[] vetor, int tamanho) {
+		BubbleSort bubblesort = new BubbleSort();
+		MergeSort mergesort = new MergeSort();
+
+		int[] bubble = bubblesort.main(vetor, tamanho);
+		int[] merge = mergesort.main(vetor, tamanho);
+
+		System.out.println("BUBBLE: ");
+		for (int i = 0; i < tamanho; i++) {
+			System.out.print(bubble[i] + " ");
+		}
+
+		System.out.println("\nMERGE: ");
+		for (int i = 0; i < tamanho; i++) {
+			System.out.print(merge[i] + " ");
+		}
+	}
 
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,18 @@
+/*
+RESUMO      : Exercício 05, Execução das Ordenações
+PROGRAMADORA: Luiza Felix
+DATA        : 14/03/2023
+ */
+
+
 package view;
 
-public class Principal {
+import controller.Controller;
 
+public class Principal {
+	public static void main(String[] args) {
+		Controller teste = new Controller();
+		int[] vetor = {69, 68, 32, 36, 91, 19, 28, 44, 88, 73, 37, 99, 1};
+		teste.ordem(vetor, vetor.length);
+	}
 }
